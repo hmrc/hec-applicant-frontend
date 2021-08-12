5b8a017dbcbe038854372743626d5a09f516cb35
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2021-08-12 11:16:26 +0100

    template updated

--- app/uk/gov/hmrc/hecapplicantfrontend/models/views/LicenceTimeTradingOption.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/models/views/LicenceTimeTradingOption.scala
@@ -2,12 +2,13 @@ package uk.gov.hmrc.hecapplicantfrontend.models.views
 
 import uk.gov.hmrc.hecapplicantfrontend.models.{LicenceTimeTrading, LicenceType}
 
-final case class LicenceTimeTradingOption (messageKey: String, hintKey: Option[String])
+final case class LicenceTimeTradingOption(messageKey: String)
 object LicenceTimeTradingOption {
-  def licenceTimeTradingOption(licenceTimeTrading: LicenceTimeTrading): LicenceTypeOption = licenceTimeTrading match {
-    case LicenceTimeTrading.ZeroToTwoYears  => LicenceTypeOption("zeroToTwoYears", None)
-    case LicenceTimeTrading.TwoToFourYears     => LicenceTypeOption("twoToFourYears", None)
-    case LicenceTimeTrading.FourToEightYears          => LicenceTypeOption("fourToEightYears", None)
-    case LicenceTimeTrading.EightYearsOrMore => LicenceTypeOption("eightYearsOrMore", None)
-  }
+  def licenceTimeTradingOption(licenceTimeTrading: LicenceTimeTrading): LicenceTimeTradingOption =
+    licenceTimeTrading match {
+      case LicenceTimeTrading.ZeroToTwoYears   => LicenceTimeTradingOption("zeroToTwoYears")
+      case LicenceTimeTrading.TwoToFourYears   => LicenceTimeTradingOption("twoToFourYears")
+      case LicenceTimeTrading.FourToEightYears => LicenceTimeTradingOption("fourToEightYears")
+      case LicenceTimeTrading.EightYearsOrMore => LicenceTimeTradingOption("eightYearsOrMore")
+    }
 }
