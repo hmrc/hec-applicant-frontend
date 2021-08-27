d18179d6e41cec367e5504001a2f2c367614312b
Meghana Viswanath <13031285+meghana-viswanath@users.noreply.github.com>
2021-08-27 14:31:04 +0100

    HEC-790 Display Applicant's Tax Check Code (#18)
    Tax check complete API
    Co-authored-by: Alistair Macdonald <1460971+nubz@users.noreply.github.com>

---
+++ test/uk/gov/hmrc/hecapplicantfrontend/controllers/TaxCheckCompleteControllerSpec.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hecapplicantfrontend.controllers
+
+import java.time.LocalDate
+
+import play.api.inject.bind
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.hecapplicantfrontend.models.RetrievedApplicantData.{CompanyRetrievedData, IndividualRetrievedData}
+import uk.gov.hmrc.hecapplicantfrontend.models._
+import uk.gov.hmrc.hecapplicantfrontend.models.ids.{GGCredId, NINO}
+import uk.gov.hmrc.hecapplicantfrontend.models.licence.LicenceType
+import uk.gov.hmrc.hecapplicantfrontend.repos.SessionStore
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class TaxCheckCompleteControllerSpec
+    extends ControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with AuthAndSessionDataBehaviour
+    with JourneyServiceSupport {
+
+  override def overrideBindings = List(
+    bind[AuthConnector].toInstance(mockAuthConnector),
+    bind[SessionStore].toInstance(mockSessionStore)
+  )
+
+  val controller = instanceOf[TaxCheckCompleteController]
+
+  val individualRetrievedData =
+    IndividualRetrievedData(GGCredId(""), NINO(""), None, Name("", ""), DateOfBirth(LocalDate.now()), None)
+
+  val companyRetrievedData =
+    CompanyRetrievedData(GGCredId(""), None, None)
+
+  "TaxCheckCompleteController" when {
+
+    "handling request to tax situation page " must {
+
+      def performAction(): Future[Result] = controller.taxCheckComplete(FakeRequest())
+
+      behave like authAndSessionDataBehaviour(performAction)
+
+      "return an InternalServerError" when {
+
+        "a tax check code cannot be found in session" in {
+          val session = HECSession(individualRetrievedData, UserAnswers.empty, None)
+
+          inSequence {
+            mockAuthWithNoRetrievals()
+            mockGetSession(session)
+          }
+
+          status(performAction()) shouldBe INTERNAL_SERVER_ERROR
+        }
+
+      }
+
+      "display the page" when {
+
+        "tax check code has been generated for the user " in {
+          val taxCheckCode = "LXB7G6DX7"
+          val expiryDate   = LocalDate.of(2020, 1, 8)
+          val session      = HECSession(
+            individualRetrievedData,
+            UserAnswers.empty.copy(
+              licenceType = Some(LicenceType.DriverOfTaxisAndPrivateHires)
+            ),
+            Some(HECTaxCheck(HECTaxCheckCode(taxCheckCode), expiryDate))
+          )
+
+          inSequence {
+            mockAuthWithNoRetrievals()
+            mockGetSession(session)
+          }
+
+          checkPageIsDisplayed(
+            performAction(),
+            messageFromMessageKey("taxCheckComplete.title"),
+            doc => {
+              doc.select(".govuk-panel__body").text should include regex "LXB 7G6 DX7"
+
+              doc
+                .select(".govuk-body")
+                .html should include regex messageFromMessageKey("taxCheckComplete.p2", "8 January 2020")
+            }
+          )
+        }
+
+      }
+
+    }
+  }
+
+}
