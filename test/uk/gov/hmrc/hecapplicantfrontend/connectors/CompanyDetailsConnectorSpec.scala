28e6afe6c17071c8fc9d79928fc7b48ffb71d66f
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2021-09-20 15:40:27 +0100

    HEC-1060 connector and service for company details (#34)
    * HEC-1060 connector and service for company details
    
    * HEC-1060 code refactor

---
+++ test/uk/gov/hmrc/hecapplicantfrontend/connectors/CompanyDetailsConnectorSpec.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hecapplicantfrontend.connectors
+
+import com.typesafe.config.ConfigFactory
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.Configuration
+import uk.gov.hmrc.hecapplicantfrontend.models.ids.CRN
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class CompanyDetailsConnectorSpec
+    extends AnyWordSpec
+    with Matchers
+    with MockFactory
+    with HttpSupport
+    with ConnectorSpec {
+
+  val (protocol, host, port) = ("http", "host", "123")
+
+  val config    = Configuration(
+    ConfigFactory.parseString(s"""
+                                 | microservice.services.companies-house-proxy {
+                                 |    protocol = "$protocol"
+                                 |    host     = "$host"
+                                 |    port     = $port
+                                 |  }
+                                 |""".stripMargin)
+  )
+  val connector = new CompanyDetailsConnectorImpl(mockHttp, new ServicesConfig(config))
+
+  "CompanyDetailsConnectorImpl" when {
+
+    "handling requests to get company details" must {
+
+      implicit val hc: HeaderCarrier = HeaderCarrier()
+
+      val companyNumber = CRN("1234567")
+
+      val expectedUrl = s"$protocol://$host:$port/companies-house-api-proxy/company/1234567"
+
+      behave like connectorBehaviour(
+        mockGet[HttpResponse](expectedUrl)(_),
+        () => connector.findCompany(companyNumber)
+      )
+
+    }
+
+  }
+
+}
---
+++ test/uk/gov/hmrc/hecapplicantfrontend/services/CompanyDetailsServiceImplSpec.scala
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hecapplicantfrontend.services
+
+import cats.data.EitherT
+import cats.instances.future._
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import uk.gov.hmrc.hecapplicantfrontend.connectors.CompanyDetailsConnector
+import uk.gov.hmrc.hecapplicantfrontend.models.{CompanyHouseDetails, CompanyHouseName, Error}
+import uk.gov.hmrc.hecapplicantfrontend.models.ids.CRN
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class CompanyDetailsServiceImplSpec extends AnyWordSpec with Matchers with MockFactory {
+
+  val mockCompanyDetailsConnector = mock[CompanyDetailsConnector]
+
+  def mockFindComapny(companyNumber: CRN)(response: Either[Error, HttpResponse]) =
+    (mockCompanyDetailsConnector
+      .findCompany(_: CRN)(_: HeaderCarrier))
+      .expects(companyNumber, *)
+      .returning(EitherT.fromEither[Future](response))
+
+  val companyDetailsService      = new CompanyDetailsServiceImpl(mockCompanyDetailsConnector)
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  "CompanyDetailsServiceImpl" when {
+
+    "handling requests to get citizen details" must {
+
+      val responseHeaders = Map.empty[String, Seq[String]]
+
+      "return an error" when {
+
+        def testIsError(mockAction: CRN => Unit): Unit = {
+          val companyNumber = CRN("1234567")
+
+          mockAction(companyNumber)
+
+          await(companyDetailsService.findCompany(companyNumber).value) shouldBe a[Left[_, _]]
+        }
+
+        "the call to get company details fails" in {
+          testIsError(mockFindComapny(_)(Left(Error(""))))
+        }
+
+        "the call to get company details comes back with a 500 response" in {
+          testIsError(mockFindComapny(_)(Right(HttpResponse(500, ""))))
+        }
+
+        "the call to get company details comes back with a 404 response" in {
+          testIsError(mockFindComapny(_)(Right(HttpResponse(404, ""))))
+        }
+
+        "there is no JSON in the response body" in {
+          testIsError(mockFindComapny(_)(Right(HttpResponse(200, ""))))
+        }
+
+        "the JSON cannot be parsed in the response body" in {
+          testIsError(
+            mockFindComapny(_)(
+              Right(
+                HttpResponse(
+                  200,
+                  Json.parse("""{ "a" : "b" }"""),
+                  responseHeaders
+                )
+              )
+            )
+          )
+        }
+
+      }
+
+      "return the details found" when {
+
+        "the response from company details is OK and the json body can be parsed" in {
+
+          val crn            = CRN("1234567")
+          val companyDetails = CompanyHouseDetails(CompanyHouseName("Test Tech Ltd"))
+
+          val json = Json.parse(s"""
+               |{"company_name" : "Test Tech Ltd"}
+               |""".stripMargin)
+
+          mockFindComapny(CRN("1234567"))(Right(HttpResponse(200, json, responseHeaders)))
+          val result = companyDetailsService.findCompany(crn)
+          await(result.value) shouldBe Right(companyDetails)
+        }
+      }
+
+    }
+
+  }
+
+}
