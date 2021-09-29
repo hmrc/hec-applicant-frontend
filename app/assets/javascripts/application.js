7e71d49fe94f0431c4ca83c3c886dd2a276a3aca
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2021-09-29 09:58:31 +0100

    add new components, JS and Sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,32 @@
-(function (document, window) {
+(function (document, window, navigator) {
+
+  // copy buttons
+  (function (document, navigator) {
+    const activeClassName = 'copied-to-clipboard'
+    function copy(event) {
+      const el = event.currentTarget
+      return navigator.clipboard.writeText(el.dataset.clip).then(function () {
+          resetCopyButtons()
+          el.classList.add(activeClassName)
+          el.blur()
+        }, function (e) {
+          console.error(e)
+        }
+      )
+    }
+    function resetCopyButtons() {
+      document.querySelectorAll(`button.${activeClassName}`)
+        .forEach(function (el) {
+          el.classList.remove(activeClassName)
+        })
+    }
+    document.querySelectorAll('button.copy-to-clipboard')
+      .forEach(function (el) {
+        el.addEventListener('click', copy)
+      })
+  })(document, navigator)
+  // end copy buttons
+
   document.querySelectorAll('a[href="#print-dialogue"]')
     .forEach(function(link) {
       link.addEventListener('click', function(event) {
@@ -6,4 +34,4 @@
         window.print();
       })
     })
-})(document, window);
+})(document, window, navigator);
