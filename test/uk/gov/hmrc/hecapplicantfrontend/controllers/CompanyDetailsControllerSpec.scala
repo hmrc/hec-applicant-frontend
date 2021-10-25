e1974aa13de432bd29a59fa5675e8ea833d5fba9
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2021-10-25 10:36:16 +0100

    add unit test

--- test/uk/gov/hmrc/hecapplicantfrontend/controllers/CompanyDetailsControllerSpec.scala
+++ test/uk/gov/hmrc/hecapplicantfrontend/controllers/CompanyDetailsControllerSpec.scala
@@ -1644,6 +1644,37 @@ class CompanyDetailsControllerSpec
 
     }
 
+    "handling requests to the 'CT UTR not matched' page" must {
+
+      def performAction() = controller.ctutrNotMatched(FakeRequest())
+
+      behave like authAndSessionDataBehaviour(performAction)
+
+      "display the page" in {
+        val session = Fixtures.companyHECSession()
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+          mockJourneyServiceGetPrevious(routes.CompanyDetailsController.ctutrNotMatched(), session)(mockPreviousCall)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey("ctutrNotMatched.title"),
+          { doc =>
+            doc.select("#back").attr("href") shouldBe mockPreviousCall.url
+            val links = doc.select(".govuk-body > .govuk-link")
+            links.iterator().asScala.toList.map(_.attr("href")) shouldBe List(
+              appConfig.signOutUri,
+              appConfig.createGGAccount
+            )
+          }
+        )
+
+      }
+
+    }
+
   }
 
 }
