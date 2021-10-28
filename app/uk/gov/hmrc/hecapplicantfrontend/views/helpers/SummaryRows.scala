9422f60f631f3de82d183a095a2c94e44a82ef55
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2021-10-28 14:58:36 +0100

    abstract all summary rows to helper

--- app/uk/gov/hmrc/hecapplicantfrontend/views/helpers/SummaryRows.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/views/helpers/SummaryRows.scala
@@ -20,8 +20,16 @@ import play.api.i18n.Messages
 import play.api.mvc.Call
 import uk.gov.hmrc.govukfrontend.views.html.components._
 import uk.gov.hmrc.hecapplicantfrontend.controllers.routes
-import uk.gov.hmrc.hecapplicantfrontend.models.CompleteUserAnswers
+import uk.gov.hmrc.hecapplicantfrontend.models.CompanyUserAnswers.CompleteCompanyUserAnswers
+import uk.gov.hmrc.hecapplicantfrontend.models.{CompleteUserAnswers, RetrievedJourneyData}
 import uk.gov.hmrc.hecapplicantfrontend.models.views.{LicenceTimeTradingOption, LicenceTypeOption, LicenceValidityPeriodOption}
+import uk.gov.hmrc.hecapplicantfrontend.models.CTStatusResponse
+import uk.gov.hmrc.hecapplicantfrontend.models.IndividualUserAnswers.CompleteIndividualUserAnswers
+import uk.gov.hmrc.hecapplicantfrontend.models.RetrievedJourneyData.CompanyRetrievedJourneyData
+import uk.gov.hmrc.hecapplicantfrontend.util.TimeUtils
+import uk.gov.hmrc.hecapplicantfrontend.models.views.EntityTypeOption
+import uk.gov.hmrc.hecapplicantfrontend.models.views.YesNoOption
+import uk.gov.hmrc.hecapplicantfrontend.models.views.TaxSituationOption
 
 class SummaryRows {
   val messageKey = "checkYourAnswers"
@@ -82,4 +90,116 @@ class SummaryRows {
       licenceValidityPeriodRow
     )
   }
+
+  def companyRows(completeAnswers: CompleteCompanyUserAnswers, retrievedJourneyData: RetrievedJourneyData)(implicit
+    messages: Messages
+  ): List[SummaryListRow] = {
+
+    val entityTypeRow             =
+      summaryListRow(
+        messages("entityType.title"),
+        messages(s"entityType.${EntityTypeOption.entityTypeOption(completeAnswers.entityType).messageKey}"),
+        routes.EntityTypeController.entityType(),
+        messages(s"$messageKey.entityType.screenReaderText")
+      )
+    val crnRow                    =
+      summaryListRow(
+        messages("crn.title"),
+        completeAnswers.crn.value,
+        routes.CRNController.companyRegistrationNumber(),
+        messages(s"$messageKey.crn.screenReaderText")
+      )
+    val ctutrRow                  =
+      completeAnswers.ctutr.map { ctutr =>
+        summaryListRow(
+          messages("enterCtutr.title"),
+          ctutr.value,
+          routes.CompanyDetailsController.enterCtutr(),
+          messages(s"$messageKey.ctutr.screenReaderText")
+        )
+      }
+    val recentlyStartedTradingRow =
+      completeAnswers.recentlyStartedTrading.map { recentlyStartedTrading =>
+        summaryListRow(
+          messages("recentlyStartedTrading.title"),
+          messages(s"recentlyStartedTrading.${YesNoOption.yesNoOption(recentlyStartedTrading).messageKey}"),
+          routes.CompanyDetailsController.recentlyStartedTrading(),
+          messages(s"$messageKey.recentlyStartedTrading.screenReaderText")
+        )
+      }
+
+    val chargeableForCTRow  =
+      completeAnswers.chargeableForCT.map { chargeableForCT =>
+        val latestAccountingPeriod = retrievedJourneyData match {
+          case c: CompanyRetrievedJourneyData =>
+            c.ctStatus match {
+              case Some(CTStatusResponse(_, _, _, Some(latestAccountingPeriod))) => latestAccountingPeriod
+              case _                                                             => sys.error("chargeableForCT answer found when no accounting period")
+            }
+          case _                              => sys.error("chargeableForCT answer found when company journey data not retrieved")
+        }
+        summaryListRow(
+          messages("chargeableForCT.title", TimeUtils.govDisplayFormat(latestAccountingPeriod.endDate)),
+          messages(s"chargeableForCT.${YesNoOption.yesNoOption(chargeableForCT).messageKey}"),
+          routes.CompanyDetailsController.chargeableForCorporationTax(),
+          messages(s"$messageKey.chargeableForCT.screenReaderText")
+        )
+
+      }
+    val ctIncomeDeclaredRow =
+      completeAnswers.ctIncomeDeclared.map { ctIncomeDeclared =>
+        summaryListRow(
+          messages("ctIncomeDeclared.title"),
+          messages(s"ctIncomeDeclared.${YesNoOption.yesNoOption(ctIncomeDeclared).messageKey}"),
+          routes.CompanyDetailsController.ctIncomeStatement(),
+          messages(s"$messageKey.ctIncomeDeclared.screenReaderText")
+        )
+      }
+
+    List(
+      Some(entityTypeRow),
+      Some(crnRow),
+      ctutrRow,
+      recentlyStartedTradingRow,
+      chargeableForCTRow,
+      ctIncomeDeclaredRow
+    ).collect { case Some(r) => r }
+  }
+
+  def individualRows(
+    completeAnswers: CompleteIndividualUserAnswers
+  )(implicit messages: Messages): List[SummaryListRow] = {
+    val entityTypeRow       =
+      completeAnswers.entityType.map { entityType =>
+        summaryListRow(
+          messages("entityType.title"),
+          messages(s"entityType.${EntityTypeOption.entityTypeOption(entityType).messageKey}"),
+          routes.EntityTypeController.entityType(),
+          messages(s"$messageKey.entityType.screenReaderText")
+        )
+      }
+    val taxSituationRow     =
+      summaryListRow(
+        messages("taxSituation.title"),
+        messages(s"taxSituation.${TaxSituationOption.taxSituationOption(completeAnswers.taxSituation).messageKey}"),
+        routes.TaxSituationController.taxSituation(),
+        messages(s"$messageKey.taxSituation.screenReaderText")
+      )
+    val saIncomeDeclaredRow =
+      completeAnswers.saIncomeDeclared.map { saIncomeDeclared =>
+        summaryListRow(
+          messages("saIncomeDeclared.title"),
+          messages(s"saIncomeDeclared.${YesNoOption.yesNoOption(saIncomeDeclared).messageKey}"),
+          routes.SAController.saIncomeStatement(),
+          messages(s"$messageKey.saIncomeDeclared.screenReaderText")
+        )
+      }
+
+    List(
+      entityTypeRow,
+      Some(taxSituationRow),
+      saIncomeDeclaredRow
+    ).collect { case Some(r) => r }
+  }
+
 }
