3d65b240cde9b3b46e1f2f8992277b33451fd511
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2021-11-03 08:52:42 +0000

    journey starter

---
+++ app/uk/gov/hmrc/hecapplicantfrontend/testonly/services/AuthLoginStubService.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hecapplicantfrontend.testonly.services
+
+import cats.data.EitherT
+import cats.syntax.eq._
+import com.google.inject.{ImplementedBy, Inject, Singleton}
+import play.api.http.Status._
+import uk.gov.hmrc.hecapplicantfrontend.models.Error
+import uk.gov.hmrc.hecapplicantfrontend.testonly.connectors.AuthLoginStubConnector
+import uk.gov.hmrc.hecapplicantfrontend.testonly.models.{HttpHeaders, LoginData}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@ImplementedBy(classOf[AuthLoginStubServiceImpl])
+trait AuthLoginStubService {
+
+  def login(loginData: LoginData)(implicit hc: HeaderCarrier): EitherT[Future, Error, HttpHeaders]
+
+}
+
+@Singleton
+class AuthLoginStubServiceImpl @Inject() (connector: AuthLoginStubConnector)(implicit ec: ExecutionContext)
+    extends AuthLoginStubService {
+
+  override def login(
+    loginData: LoginData
+  )(implicit hc: HeaderCarrier): EitherT[Future, Error, HttpHeaders] =
+    connector.login(loginData).subflatMap { httpResponse =>
+      val status        = httpResponse.status
+      lazy val location = httpResponse.header("location")
+      if (status =!= SEE_OTHER) {
+        Left(
+          Error(
+            s"Call to login came back with status $status. Body was ${httpResponse.body}"
+          )
+        )
+      } else if (!location.contains(loginData.redirectUrl))
+        Left(Error(s"Location header value [${location.getOrElse("")}] of response was not ${loginData.redirectUrl}"))
+      else {
+        Right(HttpHeaders(httpResponse.headers))
+      }
+    }
+
+}
