5a2a717e76110b7f1eb4d539c3aeb180192fad22
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2021-12-21 10:47:38 +0000

    confirm email address

--- app/uk/gov/hmrc/hecapplicantfrontend/controllers/ConfirmEmailAddressController.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/controllers/ConfirmEmailAddressController.scala
@@ -17,11 +17,16 @@
 package uk.gov.hmrc.hecapplicantfrontend.controllers
 
 import com.google.inject.{Inject, Singleton}
+import play.api.data.Form
+import play.api.data.Forms.{mapping, of}
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.hecapplicantfrontend.controllers.ConfirmEmailAddressController.{emailAddressForm, getEmailOptions}
 import uk.gov.hmrc.hecapplicantfrontend.controllers.actions.{AuthAction, SessionDataAction}
+import uk.gov.hmrc.hecapplicantfrontend.models.EmailAddress
 import uk.gov.hmrc.hecapplicantfrontend.services.JourneyService
-import uk.gov.hmrc.hecapplicantfrontend.util.Logging
+import uk.gov.hmrc.hecapplicantfrontend.util.{FormUtils, Logging}
+import uk.gov.hmrc.hecapplicantfrontend.views.html
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 @Singleton
@@ -29,16 +34,40 @@ class ConfirmEmailAddressController @Inject() (
   authAction: AuthAction,
   sessionDataAction: SessionDataAction,
   journeyService: JourneyService,
+  confirmEmailAddressPage: html.ConfirmEmailAddress,
   mcc: MessagesControllerComponents
 ) extends FrontendController(mcc)
     with I18nSupport
     with Logging {
 
   val confirmEmailAddress: Action[AnyContent] = authAction.andThen(sessionDataAction) { implicit request =>
-    Ok(
-      s"session: ${request.sessionData}, back :: ${journeyService.previous(routes.ConfirmEmailAddressController.confirmEmailAddress)}"
-    )
+    val back         = journeyService.previous(routes.ConfirmEmailAddressController.confirmEmailAddress)
+    val emailOpt     = request.sessionData.fold(_.loginData.emailAddress, _.loginData.emailAddress)
+    val emailOptions = getEmailOptions(emailOpt)
 
+    val form: Form[EmailAddress] = {
+      val emptyForm: Form[EmailAddress] = emailAddressForm(emailOptions)
+      emailOpt.fold(emptyForm)(emptyForm.fill)
+    }
+    Ok(confirmEmailAddressPage(form, back, emailOptions))
   }
 
+  val confirmEmailAddressSubmit: Action[AnyContent] = authAction.andThen(sessionDataAction).async { implicit request =>
+    Ok(s"${request.sessionData}")
+  }
+
+}
+
+object ConfirmEmailAddressController {
+
+  def getEmailOptions(emailAddress: Option[EmailAddress]): List[EmailAddress] = emailAddress match {
+    case Some(email) => List(email, EmailAddress("different"))
+    case None        => sys.error(" Email address is not present in user's login data.")
+  }
+
+  def emailAddressForm(options: List[EmailAddress]): Form[EmailAddress] = Form(
+    mapping(
+      "confirmEmailAddress" -> of(FormUtils.radioFormFormatter(options))
+    )(identity)(Some(_))
+  )
 }
