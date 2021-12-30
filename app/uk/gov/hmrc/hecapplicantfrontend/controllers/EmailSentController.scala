74f4787c3853bd77cf4b7f08d951518ae8e93ba9
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2021-12-30 16:15:07 +0000

    HEC-1633 email sent page

--- app/uk/gov/hmrc/hecapplicantfrontend/controllers/EmailSentController.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/controllers/EmailSentController.scala
@@ -19,23 +19,24 @@ package uk.gov.hmrc.hecapplicantfrontend.controllers
 import com.google.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.hecapplicantfrontend.controllers.VerifyEmailPasscodeController.fetchUserSelectedEmail
 import uk.gov.hmrc.hecapplicantfrontend.controllers.actions.{AuthAction, SessionDataAction}
-import uk.gov.hmrc.hecapplicantfrontend.services.JourneyService
+import uk.gov.hmrc.hecapplicantfrontend.views.html
 import uk.gov.hmrc.hecapplicantfrontend.util.Logging
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 class EmailSentController @Inject() (
   authAction: AuthAction,
   sessionDataAction: SessionDataAction,
-  journeyService: JourneyService,
+  emailSentPage: html.EmailSent,
   mcc: MessagesControllerComponents
 ) extends FrontendController(mcc)
     with I18nSupport
     with Logging {
 
   val emailSent: Action[AnyContent] = authAction.andThen(sessionDataAction) { implicit request =>
-    Ok(
-      s"session: ${request.sessionData}, back :: ${journeyService.previous(routes.EmailSentController.emailSent)}"
-    )
+    val userSelectedEmail = fetchUserSelectedEmail(request.sessionData)
+    Ok(emailSentPage(userSelectedEmail))
   }
+
 }
---
+++ test/uk/gov/hmrc/hecapplicantfrontend/controllers/EmailSentControllerSpec.scala
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.hecapplicantfrontend.controllers
+
+import cats.implicits.catsSyntaxOptionId
+import play.api.inject.bind
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.hecapplicantfrontend.models.{EmailAddress, HECSession}
+import uk.gov.hmrc.hecapplicantfrontend.models.emailVerification.{Passcode, PasscodeRequestResult, PasscodeVerificationResult}
+import uk.gov.hmrc.hecapplicantfrontend.repos.SessionStore
+import uk.gov.hmrc.hecapplicantfrontend.utils.Fixtures
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class EmailSentControllerSpec
+    extends ControllerSpec
+    with AuthSupport
+    with SessionSupport
+    with AuthAndSessionDataBehaviour {
+
+  override def overrideBindings = List(
+    bind[AuthConnector].toInstance(mockAuthConnector),
+    bind[SessionStore].toInstance(mockSessionStore)
+  )
+
+  val controller      = instanceOf[EmailSentController]
+  val ggEmailId       = EmailAddress("user@test.com")
+  val userEmailAnswer = Fixtures
+    .userEmailAnswers(
+      passcodeRequestResult = PasscodeRequestResult.PasscodeSent.some,
+      passcode = Passcode("HHHHHH").some,
+      passcodeVerificationResult = PasscodeVerificationResult.Match.some
+    )
+
+  "EmailSentControllerSpec" when {
+
+    "handling request to email sent page" must {
+      def performAction(): Future[Result] = controller.emailSent(FakeRequest())
+
+      "return a technical error page" when {
+
+        "user selected email is not in session" in {
+
+          val session = Fixtures.individualHECSession(
+            userAnswers = Fixtures.completeIndividualUserAnswers(),
+            isEmailRequested = true,
+            userEmailAnswers = Fixtures.userEmailAnswers(emailAddress = None).some
+          )
+
+          inSequence {
+            mockAuthWithNoRetrievals()
+            mockGetSession(session)
+          }
+          assertThrows[RuntimeException](await(performAction()))
+        }
+      }
+
+      "display the page" in {
+        val session: HECSession = Fixtures.companyHECSession(
+          loginData = Fixtures.companyLoginData(emailAddress = ggEmailId.some),
+          userAnswers = Fixtures.completeCompanyUserAnswers(),
+          isEmailRequested = true,
+          userEmailAnswers = userEmailAnswer.some
+        )
+
+        inSequence {
+          mockAuthWithNoRetrievals()
+          mockGetSession(session)
+        }
+
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey("emailSent.title"),
+          { doc =>
+            doc.select(".govuk-inset-text").text() should include regex "user@test.com"
+            doc.select(".govuk-body").html()       should include regex messageFromMessageKey(
+              "emailSent.p3",
+              routes.StartController.start().url
+            )
+
+          }
+        )
+      }
+    }
+  }
+}
