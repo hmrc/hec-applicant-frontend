0ea6eaf21472b95b1f549145f86aec549d7751ed
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2022-02-11 08:42:23 +0000

    upgrade hmrc-play-frontend to 3.4.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"    % bootStrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "1.31.0-play-28",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "3.4.0-play-28",
     "uk.gov.hmrc"                %% "domain"                        % "6.2.0-play-28",
     "org.typelevel"              %% "cats-core"                     % "2.7.0",
     "ai.x"                       %% "play-json-extensions"          % "0.42.0",
