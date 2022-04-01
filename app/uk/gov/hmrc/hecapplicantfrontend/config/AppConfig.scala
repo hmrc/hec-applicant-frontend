f2c5c8e40c8e10ea45333b06cea48d90125ed298
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2022-04-01 09:31:48 +0100

    WIP custom versions of required components for UR banner

--- app/uk/gov/hmrc/hecapplicantfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/config/AppConfig.scala
@@ -130,4 +130,6 @@ class AppConfig @Inject() (config: Configuration, contactFrontendConfig: Contact
     s"$baseUrl/feedback/$contactFormServiceIdentifier"
   }
 
+  val userRecruitmentUrl: String = config.get[String]("external-url.user-recruitment-url")
+
 }
