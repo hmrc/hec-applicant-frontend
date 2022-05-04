89538587687df3444ebd062fa7d7687e4dc1e744
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2022-05-04 16:27:57 +0100

    override fillInStackTrace

--- app/uk/gov/hmrc/hecapplicantfrontend/services/JourneyService.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/services/JourneyService.scala
@@ -71,6 +71,8 @@ object JourneyService {
 
     @SuppressWarnings(Array("org.wartremover.warts.Throw"))
     def doThrow: Nothing = throw this
+
+    override def fillInStackTrace(): Throwable = super.fillInStackTrace()
   }
 
 }
