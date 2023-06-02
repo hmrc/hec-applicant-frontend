459a88ccb88dd20bbcbe4be49e4084143a8ac46f
dnyuar <60218795+dnyuar@users.noreply.github.com>
2023-06-02 10:21:04 +0100

    csp filter config

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "7.13.0"
+  val bootStrapVersion = "7.15.0"
   val monocleVersion   = "2.1.0"
   val hmrcMongoVersion = "0.74.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"    % bootStrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "6.3.0-play-28",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "7.0.0-play-28",
     "uk.gov.hmrc"                %% "domain"                        % "8.1.0-play-28",
     "org.typelevel"              %% "cats-core"                     % "2.9.0",
     "ai.x"                       %% "play-json-extensions"          % "0.42.0",
