27bb66176194ddc3641f817d424bfcfde11001f3
Ellie Newell <ellie.newell@capgemini.com>
2023-07-13 11:12:18 +0100

    Dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,22 +3,22 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "7.15.0"
+  val bootStrapVersion = "7.19.0"
   val monocleVersion   = "2.1.0"
-  val hmrcMongoVersion = "0.74.0"
+  val hmrcMongoVersion = "1.3.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28"    % bootStrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "7.0.0-play-28",
-    "uk.gov.hmrc"                %% "domain"                        % "8.1.0-play-28",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc"            % "7.14.0-play-28",
+    "uk.gov.hmrc"                %% "domain"                        % "8.3.0-play-28",
     "org.typelevel"              %% "cats-core"                     % "2.9.0",
     "ai.x"                       %% "play-json-extensions"          % "0.42.0",
     "com.github.kxbmap"          %% "configs"                       % "0.6.1",
     "com.github.julien-truffaut" %% "monocle-core"                  % monocleVersion,
     "com.github.julien-truffaut" %% "monocle-macro"                 % monocleVersion,
-    "uk.gov.hmrc"                %% "emailaddress"                  % "3.7.0",
-    "uk.gov.hmrc"                %% "play-conditional-form-mapping" % "1.12.0-play-28"
+    "uk.gov.hmrc"                %% "emailaddress"                  % "3.8.0",
+    "uk.gov.hmrc"                %% "play-conditional-form-mapping" % "1.13.0-play-28"
   )
 
   val test = Seq(
