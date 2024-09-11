ada0e8224335475af3995d990a2d9844883a702f
VJWatson <171040641+VJWatson@users.noreply.github.com>
2024-09-11 18:11:08 +0100

    5 failing suites

--- app/uk/gov/hmrc/hecapplicantfrontend/config/ErrorHandler.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/config/ErrorHandler.scala
@@ -19,25 +19,24 @@ package uk.gov.hmrc.hecapplicantfrontend.config
 import javax.inject.{Inject, Singleton}
 import play.api.i18n.MessagesApi
 import play.api.mvc.Results.Redirect
-import play.api.mvc.{Request, RequestHeader, Result}
-import play.twirl.api.Html
+import play.api.mvc.{RequestHeader, Result}
+import scala.concurrent.{ExecutionContext, Future}
 import uk.gov.hmrc.hecapplicantfrontend.controllers.routes
 import uk.gov.hmrc.hecapplicantfrontend.services.JourneyService.InconsistentSessionState
 import uk.gov.hmrc.hecapplicantfrontend.util.Logging
 import uk.gov.hmrc.play.bootstrap.frontend.http.FrontendErrorHandler
 import uk.gov.hmrc.hecapplicantfrontend.views.html.ErrorTemplate
 
-import scala.concurrent.Future
-
 @Singleton
-class ErrorHandler @Inject() (errorTemplate: ErrorTemplate, val messagesApi: MessagesApi)
-    extends FrontendErrorHandler
+class ErrorHandler @Inject() (val messagesApi: MessagesApi, errorTemplate: ErrorTemplate)(implicit
+  protected val ec: ExecutionContext
+) extends FrontendErrorHandler
     with Logging {
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit
-    request: Request[_]
-  ): Html =
-    errorTemplate(pageTitle, heading, message)
+    request: RequestHeader
+  ): Future[play.twirl.api.Html] =
+    Future.successful(errorTemplate(pageTitle, heading, message))
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = exception match {
     case InconsistentSessionState(message) =>
