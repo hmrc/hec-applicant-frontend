a8bc791d32df09db06ae7431f0c9c02818b53b1d
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-06-04 14:24:45 +0100

    Apply scalafmt

--- app/uk/gov/hmrc/hecapplicantfrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/hecapplicantfrontend/config/AppConfig.scala
@@ -58,9 +58,7 @@ class AppConfig @Inject() (config: Configuration, contactFrontendConfig: Contact
   private val basGatewaySignOutUrl: String = config.get[String]("auth.sign-out.bas-gateway.url")
 
   def signOutUrl(continueUrl: Option[String]): String =
-    continueUrl.fold(basGatewaySignOutUrl)(continue =>
-      s"$signOutUrlBase?continue=${continue.urlEncode}"
-    )
+    continueUrl.fold(basGatewaySignOutUrl)(continue => s"$signOutUrlBase?continue=${continue.urlEncode}")
 
   lazy val signOutAndSignBackInUrl: String =
     signOutUrl(continueUrl = Some(s"$selfBaseUrl${routes.StartController.start.url}"))
