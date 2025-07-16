c5534324d558a1c2d5959245c31efe943794fc58
Bon Parker <bonnie.parker@digital.hmrc.gov.uk>
2025-07-16 10:37:25 +0100

    DDCYLS-7037: App Dependency Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootStrapVersion = "9.13.0"
+  val bootStrapVersion = "9.16.0"
   val monocleVersion   = "2.1.0"
   val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
-    "uk.gov.hmrc"                %% "domain-play-30"                             % "10.0.0",
+    "uk.gov.hmrc"                %% "domain-play-30"                             % "12.1.0",
     "org.typelevel"              %% "cats-core"                                  % "2.10.0",
     "com.github.julien-truffaut" %% "monocle-core"                               % monocleVersion,
     "com.github.julien-truffaut" %% "monocle-macro"                              % monocleVersion,
     "uk.gov.hmrc"                %% "play-conditional-form-mapping-play-30"      % "3.3.0",
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"                 % "12.6.0"
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30"                 % "12.7.0"
   )
 
   val test: Seq[ModuleID] = Seq(
